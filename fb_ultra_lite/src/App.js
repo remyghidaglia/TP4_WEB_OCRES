10f03b3a78c85caeba560b678cefc8d4231182b5
remyghidaglia <remycaousou@gmail.com>
2020-10-23 19:34:27 +0200

    buttons

--- fb_ultra_lite/src/App.js
+++ fb_ultra_lite/src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const PROFILS = [
+  {
+    name: "Bob"
+  },
+  {
+    name: "Martine"
+  },
+  {
+    name: "Camille"
+  }
+];
+const App = () => {
+
+  const buttons = PROFILS.map(c => <Button content={c.name}></Button>)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { buttons }
     </div>
   );
 }
+
+
+const Button = ({content}) => <button className="button">{content}</button>
 
 export default App;

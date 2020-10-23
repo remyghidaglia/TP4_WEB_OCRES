e30e324c503015fee1dc36701a09247d2473f65c
remyghidaglia <remycaousou@gmail.com>
2020-10-24 00:09:51 +0200

    callback function affichage profile

--- fb_ultra_lite/src/App.js
+++ fb_ultra_lite/src/App.js
@@ -4,42 +4,69 @@ import { useState } from 'react';
 
 const PROFILS = [
   {
-    name: "Bob"
+    firstname: "Bob",
+    name: "Dylan",
+    dateOfBirth:"17/02/1970",
+    img :"./img/bob.jpg",
   },
   {
-    name: "Martine"
+    firstname: "Martine",
+    name: "Aubry",
+    dateOfBirth:"13/03/1983",
+    img :"./img/martine.jpg",
+
   },
   {
-    name: "Camille"
+    firstname: "Camille",
+    name: "Mares",
+    dateOfBirth:"28/06/1992",
+    img :"./img/camille.jpg",
   }
 ];
-const App = () => {
 
-  const buttons = PROFILS.map(p => <Button content={p.name}></Button>)
+const App = () => {
+  // States
+  const [currentProfile, setCurrentProfile] = useState(PROFILS[0])
+
+  // Utils
+  const handleProfileClick = firstname => {
+    const profileClicked = PROFILS.filter(p => p.firstname == firstname)[0]
+    setCurrentProfile(profileClicked)
+  }
+
+  const buttons = PROFILS.map(p => <Button content={p.firstname} callback={handleProfileClick}></Button>)
 
   return (
     <div className="App">
       { buttons }
       <Button clickable content="J'aime"></Button>
+      <br />
+      {JSON.stringify(currentProfile)}
     </div>
   );
 }
 
 
-const Button = ({content, clickable}) => {
+const Button = ({content, clickable, callback}) => {
   const [clicked, setClicked] = useState(false);  
   var renderedText = content
+  var handleClick;
   if (clickable){
     renderedText = `${clicked ? 1 : 0} ` + renderedText
+    handleClick = () => setClicked(!clicked)
+  } else if (callback) {
+    handleClick = () => callback(content)
   }
+
   return (
     <button 
       className="button" 
-      onClick={clickable && (() => setClicked(!clicked))}
+      onClick={handleClick}
     >
       {renderedText} 
     </button>
     )
 }
+
 
 export default App;
